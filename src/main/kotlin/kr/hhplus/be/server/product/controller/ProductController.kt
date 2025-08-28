c7faa612cc6c5bf51b3b796993154b12a0c7c0cf
shinhansol <wkdqhrh010@naver.com>
2025-08-28 17:02:20 +0900

    getTopBestSellers swagger 설정

--- src/main/kotlin/kr/hhplus/be/server/product/controller/ProductController.kt
+++ src/main/kotlin/kr/hhplus/be/server/product/controller/ProductController.kt
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import kr.hhplus.be.server.product.controller.dto.ProductDetailRes
+import kr.hhplus.be.server.product.controller.dto.ProductListRes
 import kr.hhplus.be.server.product.service.ProductService
 import org.springframework.http.ProblemDetail
 import org.springframework.web.bind.annotation.GetMapping
@@ -72,6 +73,36 @@ import org.springframework.web.bind.annotation.RestController
 class ProductController(
     private val productService: ProductService
 ) {
+    @GetMapping("/list")
+    @Operation(
+        summary = "상품 리스트 조회",
+        description = "상품 리스트를 조회합니다."
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "상품 리스트 조회 성공",
+        content = [Content(schema = Schema(implementation = ProductListRes::class))]
+    )
+    fun getProductList(): ProductListRes {
+        return productService.getProductList()
+    }
+
+    @GetMapping("/best-sellers")
+    @Operation(
+        summary = "최근 베스트셀러 Top 3 조회",
+        description = "최근 판매량이 많은 상위 3개 상품을 조회합니다."
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "베스트셀러 조회 성공",
+        content = [Content(schema = Schema(implementation = ProductListRes::class))]
+    )
+    fun getTop3BestSellers(
+        @Parameter(description = "최근 N일(기본 7일), 1~90 사이", example = "7")
+    ): ProductListRes {
+        return productService.getTopBestSellers()
+    }
+
     @GetMapping("/{productId}")
     @Operation(
         summary = "상품 정보 조회",
